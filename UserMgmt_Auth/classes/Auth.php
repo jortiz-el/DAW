0257175c5033127d53b1c70fe1c1b218ffdd1c6f
jortiz-el <ojoao_funkyman@hotmail.com>
2015-11-28 13:58:11 +0100

    terminando phpdocumentor

--- UserMgmt_Auth/classes/Auth.php
+++ UserMgmt_Auth/classes/Auth.php
@@ -4,51 +4,78 @@
 class Auth {
     
     /**
-     *
-     * Clase logout
-     * 
-     * @final logout
-     * @method Metodo para destriur la sesion      */
+     * [logout destruye la session]
+     * @return [no return] [solo destruye sesion]
+     */
     public function logout(){
         session_unset();
         session_destroy();
     }
-    
+    /**
+     * [login estable la sesion del usuario]
+     * @param  [string/obj] $usuario [puede recibir un objeto de la clase o un string]
+     * @return [no return]
+     */
     public function login($usuario) {
         $_SESSION['usuario'] = $usuario;
     }
-    
+   /**
+    * [usuario funcion para obtener la session de usuario]
+    * @return [obj/string] [devuelve la sesion del usuario]
+    */
     public function usuario() {
         return $_SESSION['usuario'];
     }
-    
+    /**
+     * [check funcion que verifica si hay una session usuario]
+     * @return [boolean] [devuleve la sesion o false si no la hay]
+     */
     public function check() {
         return (isset($_SESSION['usuario']));
     }
     
     public function __construct() {
     }
-
+    /**
+     * [getId metodo parta obtener el id ]
+     * @return [int] [id autonumerico]
+     */
     public function getId() {
         return $this->id;
     }
-
+    /**
+     * [getNomUsuario  metodo para obtener el nombre del usuario]
+     * @return [string] [devuelve el nombre del usuario]
+     */
     public function getNomUsuario() {
         return $this->nomUsuario;
     }
-
+    /**
+     * [setNomUsuario metodo para establecer el nombre del usuario]
+     * @param [string] $nomUsuario [nombre del usuario]
+     */
     public function setNomUsuario($nomUsuario) {
         $this->nomUsuario = $nomUsuario;
     }
-
+    /**
+     * [getClave metodo para obtener la clave del usuario]
+     * @return [string] [devulve la clave del usuario]
+     */
     public function getClave() {
         return $this->clave;
     }
-    
+    /**
+     * [setClave metodo para establecer la clave del usuario]
+     * @param [string] $clave [clave del usuario]
+     */
     public function setClave($clave) {
         $this->clave = $clave;
     }
-
+    /**
+     * [persist] funcion para insertar en la base de datos en nombre y clave 
+     * del usuario actual 
+     * @return [noreturn] [esta funcion no devuelve nada]
+     */
     public function persist() {
         $bd = BD::getConexion();
         $sqlInsertUsuario = "insert into usuarios (nomUsuario, clave) values (:nomUsuario, :clave)";
--- UserMgmt_Auth/classes/BD.php
+++ UserMgmt_Auth/classes/BD.php
@@ -1,13 +1,34 @@
 <?php
 
 class BD {
-
+    /**
+     * [$basedatos nombre de la base de datos]
+     * @var string
+     */
     private $basedatos = 'gestionusuarios';
+    /**
+     * [$usuario nombre del usuario en la base de datos]
+     * @var string
+     */
     private $usuario = 'root';
+    /**
+     * [$contrasenya contraseña para entrar a la base de datos]
+     * @var string
+     */
     private $contrasenya = '';
+    /**
+     * [$equipo direccion del servidor ]
+     * @var string
+     */
     private $equipo = 'localhost';
-    
+    /**
+     * [$bd variable estatica para conectarse a la misma clase]
+     * @var null
+     */
     protected static $bd = null;
+    /**
+     * [__construct establece la coneXion cuando se crea el objeto de la clase]
+     */
     private function __construct() {
         try {
             self::$bd = new PDO("mysql:host=$this->equipo;dbname=$this->basedatos", $this->usuario, $this->contrasenya);
@@ -16,7 +37,10 @@ class BD {
             die();
         }
     }
-
+    /**
+     * [getConexion  metodo estatico para instanciar al objeto con la conexion]
+     * @return [obj] [devuelve un objeto de la clase conexion ya conectado a la base de datos]
+     */
     public static function getConexion() {
         if (!self::$bd) {
             new BD();
